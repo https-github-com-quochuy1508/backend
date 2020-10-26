fefd1af60cf364fffa8afe2f8e3f874b2f43b594
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-10-26 15:42:36 +0700

    Api upload image and video

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import userRoutes from './routes/apiRouter/userRoutes';
 import postRoutes from './routes/apiRouter/postRoutes';
+import uploadRoutes from './routes/apiRouter/uploadRoutes';
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.get('/', (req, res) => {
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/upload', uploadRoutes);
 
 export default router;
