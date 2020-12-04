9286d3e82f1dd34011c5fac807eeced7a8034f8c
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2020-12-04 21:09:16 +0700

    Add api like

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import userRoutes from './routes/apiRouter/userRoutes';
 import postRoutes from './routes/apiRouter/postRoutes';
 import commentRoutes from './routes/apiRouter/commentRoutes';
+import likeRoutes from './routes/apiRouter/likeRoutes';
 import uploadRoutes from './routes/apiRouter/uploadRoutes';
 
 const router = Router();
@@ -18,5 +19,6 @@ router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 router.use('/upload', uploadRoutes);
+router.use('/likes', likeRoutes);
 
 export default router;
